02ce08f0b3bae37a81c2d1fa9f721de4af7ed157
olameena <yusufaminatolabimpe.com>
2022-12-08 07:59:11 -0800

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * main - print a line of code using put
+/**
+ * main - print a line of code with put
+ *
  * return: 0
 */
-int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0)
 }
