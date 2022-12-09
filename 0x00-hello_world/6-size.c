29ee3ed6ecaed396414841ea6011cb896b0bcb30
olameena <yusufaminatolabimpe.com>
2022-12-09 11:09:32 -0800

    moving aroubd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+ #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: 'the program's description'
- *
- * return: Always 0 (Success)
-*/
+ /**
+  * main - Entry point
+  *
+  * Description: 'the program's description'
+  *
+  * return: Always 0 (Success)
+ */
 
-int main(void) {
+int main(void) 
+{
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
