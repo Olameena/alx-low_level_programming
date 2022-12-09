fb66e6370d88d1e3ef361a24972570120f93151d
olameena <yusufaminatolabimpe.com>
2022-12-09 10:48:37 -0800

    resolving

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry point
  *
- * Description: using sizeof to print the size of various types.
+ * Description: 'the program's description'
  *
- * return: Always 0 (Succesful)
+ * return: Always 0 (Success)
 */
 
 int main(void)
@@ -13,10 +13,10 @@ int main(void)
 
 	/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	//printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	//printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	//printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	//printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return 0;
 }
