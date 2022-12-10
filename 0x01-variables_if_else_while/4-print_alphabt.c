7abf67209caceef4b3894c7ab562f1b946a253b3
olameena <yusufaminatolabimpe.com>
2022-12-10 10:23:46 -0800

    corrected

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -18,11 +18,11 @@ int main(void)
 
 	e = 'e';
 	q = 'q';
-	for (low = 'a'; low <= 'z'; low++
-			{
-			if (low != e && low != q)
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
 			putchar(low);
-			}
-			putchar('\n');
-			return (0);
+	}
+	putchar('\n');
+	return (0);
 }
