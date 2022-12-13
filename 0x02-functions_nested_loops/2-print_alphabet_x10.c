bc3e833640e7a404f8999d71a8a5c7e81271167f
olameena <yusufaminatolabimpe.com>
2022-12-13 07:34:17 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * main - print_alphabet_x10 - Check holberto
+ * main - print_alphabet_x10
+ *
  *  Description: function uses _putchar function to print
  *  alphabet in lowercase 10 times
  *  Return: Nothing.
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	char i;
-	int j;
-	
-	for (j = 0; j < 10; j++)
-	{
-		for (i = 'a'; i <= 'z'; i++)
-			_putchar(i);
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
-	}
 }
